566354338331e1c217cc34628143408e2abbd5bd
Kkkuc <91897@student.pb.edu.pl>
2026-02-06 15:40:03 +0100

    some folders have been added

---
+++ Calendar_Core/MatchData.cs
@@ -0,0 +1,13 @@
+﻿namespace Calendar_Core;
+
+public class MatchData(string host, string guest, int? hostSetsResult, int? guestSetsResult, DateTime? matchDate, TimeSpan? matchTime, string court)
+{
+    public string Host = host;
+    public string Guest = guest;
+    public int? HostSetsResult = hostSetsResult;
+    public int? GuestSetsResult = guestSetsResult;
+    public DateTime? MatchDate = matchDate;
+    public TimeSpan? MatchTime = matchTime;
+    public string Court = court;
+    
+}
