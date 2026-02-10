2bb0eab16228bdb05f5e7c896beb13fc3ea51fa8
Kkkuc <91897@student.pb.edu.pl>
2026-02-10 15:10:05 +0100

    Working version from static credentials

--- Calendar_CLI/Program.cs
+++ Calendar_CLI/Program.cs
@@ -4,10 +4,24 @@ namespace Calendar_CLI;
 
 internal static class Program
 {
-    private static async Task Main()
+    [Obsolete("Obsolete")]
+    private static Task Main()
     {
-        var page = new Page("https://blssiatkowka.ligspace.pl/index.php?mod=Teams&ac=TeamSchedule&t_id=18");
+        var radSquadPage = new Page("https://blssiatkowka.ligspace.pl/index.php?mod=Teams&ac=TeamSchedule&t_id=18");
         //page.ShowAllMatches();
-        page.ShowUnplayedMatches();
+        //page.ShowUnplayedMatches();
+        
+        var calendar = new GoogleCalendarHelper();
+        calendar.Logout();
+        //var date = new DateTime(2026, 2, 10, 16, 0, 0);
+        //var title = "Spotkanie z klientem";
+
+        foreach (var match in radSquadPage.UnplayedMatches)
+        {
+            calendar.AddEvent(match.MatchDate, match.ToString());
+        }
+
+        
+        return Task.CompletedTask;
     }
 }
