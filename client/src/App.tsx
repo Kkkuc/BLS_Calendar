f0c461eb5d1c1587391138419b430693304812ca
Kkkuc <91897@student.pb.edu.pl>
2026-08-24 20:02:19 +0200

    Made toggle button into switch and some corrections to logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
-import type { Team } from './types';
-import type { MatchDto } from './types/match';
+import {useState, useEffect} from 'react';
+import type {Team} from './types';
+import type {MatchDto} from './types/match';
 import TeamSelection from './components/TeamSelection';
-import { MatchList } from './components/MatchList';
-import { ExportConfirmModal, type ExportSummaryData } from './components/ExportConfirmModal';
-import { ExportSummaryModal } from './components/ExportSummaryModal';
+import {MatchList} from './components/MatchList';
+import {ExportConfirmModal, type ExportSummaryData} from './components/ExportConfirmModal';
+import {ExportSummaryModal} from './components/ExportSummaryModal';
 import './App.css';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
     const [theme, setTheme] = useState<'dark' | 'light'>(() => {
         return (localStorage.getItem('theme') as 'dark' | 'light') || 'dark';
     });
-    
+
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
@@ -47,15 +47,20 @@ export default function App() {
     return (
         <div className="app-container">
             <header className="header">
-                <button
-                    onClick={toggleTheme}
-                    className="theme-toggle-btn"
-                    title="Zmień motyw kolorystyczny"
-                >
-                    {theme === 'dark' ? '☀️ Light' : '🌙 Dark'}
-                </button>
-                
-                
+                <div className="top-bar">
+                    <label className="theme-switch" title="Zmień motyw">
+                        <input
+                            type="checkbox"
+                            onChange={toggleTheme}
+                            checked={theme === 'light'}
+                        />
+                        <span className="slider">
+                            <span className="icon">{theme === 'dark' ? '🌙' : '☀️'}</span>
+                        </span>
+                    </label>
+                </div>
+
+
                 <img
                     src="public/front_logo_BLS.jpg"
                     alt="Białostocka Liga Sportu"
@@ -66,7 +71,7 @@ export default function App() {
 
             <main>
                 {!selectedTeam ? (
-                    <TeamSelection onSelectTeam={(team) => setSelectedTeam(team)} />
+                    <TeamSelection onSelectTeam={(team) => setSelectedTeam(team)}/>
                 ) : (
                     <div className="selected-team-container">
                         <div className="card mb-4 flex justify-between items-center">
